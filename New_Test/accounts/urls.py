6b5ad6cc7bb1923e26393b5679bc7a4020ff4895
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2024-01-22 17:43:21 +0900

    accounts 완료, nav 완료, figma 메인화면완료

--- New_Test/accounts/urls.py
+++ New_Test/accounts/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("/<int:pk>/follow/", views.follow, name="follow"),
-    path("/<int:pk>/", views.detail, name="detail"), 
-    path("/signup/", views.signup, name="signup"),
-    path("/update/", views.update, name="update"),
-    path("/login/", views.login_view, name="login"),
+    path("<int:pk>/follow/", views.follow, name="follow"),
+    path("<int:pk>/", views.detail, name="detail"), 
+    path("signup/", views.signup, name="signup"),
+    path("update/", views.update, name="update"),
+    path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
 ]
--- New_Test/plant/settings.py
+++ New_Test/plant/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,15 +57,15 @@ ROOT_URLCONF = 'plant.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
@@ -121,6 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# static file들을 어디에 집합시킬 것인지 결정
+# 배포를 할때는 만들어둔 static 파일들을 총합해서 배포한다. 
+# staticfiles라는 폴더에 모든 static file들을 집합시킨다.
+STATIC_ROOT = os.path.join('staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
