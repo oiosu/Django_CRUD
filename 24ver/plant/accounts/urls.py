df7dc56687313454dcab4986da2d69612ccac3fd
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2024-01-22 11:30:30 +0900

    django-user

--- 24ver/plant/accounts/urls.py
+++ 24ver/plant/accounts/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("<int:pk>/follow/", views.follow, name="follow"),
-    path("<int:pk>/", views.detail, name="detail"),
+    path("<str:pk>/follow/", views.follow, name="follow"),
+    # path("<str:pk>/", views.detail, name="detail"),  # <str:pk>로 수정
     path("signup/", views.signup, name="signup"),
     path("update/", views.update, name="update"),
     path("login/", views.login_view, name="login"),
--- 24ver/plant/plant/urls.py
+++ 24ver/plant/plant/urls.py
@@ -8,6 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")), 
-    path("", include("pages.urls")),
+    path("home", include("pages.urls")),
     path("articles/", include("articles.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
