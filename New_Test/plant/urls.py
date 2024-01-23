ae4221d9af10d1c519062a22ac9cdacfc4ef4e18
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2024-01-23 17:21:56 +0900

    template main.html block content, endblock 해결중

--- New_Test/plant/urls.py
+++ New_Test/plant/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("pages.urls")),
     path('accounts', include("accounts.urls")),
+    path('', TemplateView.as_view(template_name="main/main.html") ,name="main"),
 ]
 
 if settings.DEBUG:
