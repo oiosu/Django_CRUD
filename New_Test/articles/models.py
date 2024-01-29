b7ebec6c33e329dd8ac4104525a7e953d3e52c54
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2024-01-29 16:08:49 +0900

    no such column 오류 해결

--- New_Test/articles/models.py
+++ New_Test/articles/models.py
@@ -4,7 +4,7 @@ from imagekit.models import ProcessedImageField
 from django.conf import settings
 
 class Article(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=20, default='')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
